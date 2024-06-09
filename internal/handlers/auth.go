63f5d81681e92b31b990cfb6ff2b78a63dd8b98a
ernstlegaspi <legaspiernstdanielle@gmail.com>
2024-06-09 09:56:05 +0800

    validate data and error handling

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"gochat/internal/utils"
 	"net/http"
+	"strings"
 )
 
 type auth struct {
@@ -23,11 +24,31 @@ func (a *auth) InitAuthAPI() {
 
 func (a *auth) register(w http.ResponseWriter, r *http.Request) {
 	var email string = r.FormValue("email")
-	var name string = r.FormValue("name")
+	var name string = strings.TrimSpace(r.FormValue("name"))
 	var password string = r.FormValue("password")
 
 	if email == "" || name == "" || password == "" {
 		utils.AlertError(w, "All Fields are required.")
+		return
+	}
+
+	if !utils.LettersAndSpaces(name) {
+		utils.AlertError(w, "Name should be letters only.")
+		return
+	}
+
+	if len(name) < 4 {
+		utils.AlertError(w, "Name should be 4 or more characters.")
+		return
+	}
+
+	if len(email) < 7 {
+		utils.AlertError(w, "Email should be 8 characters or more.")
+		return
+	}
+
+	if len(password) < 7 {
+		utils.AlertError(w, "Password should be 8 characters or more.")
 		return
 	}
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"html/template"
 	"net/http"
+	"regexp"
 )
 
 func AlertError(w http.ResponseWriter, message string) {
@@ -15,4 +16,9 @@ func AlertSuccess(w http.ResponseWriter, message string) {
 	w.Header().Set("HX-Target", "#success-message")
 	templ := template.Must(template.ParseFiles("../internal/views/alerts/success.html"))
 	templ.Execute(w, map[string]string{"message": message})
+}
+
+func LettersAndSpaces(str string) bool {
+	regex, _ := regexp.Compile("^[a-zA-Z ]+$")
+	return regex.MatchString(str)
 }
